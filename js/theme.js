7840295b8b9030f7cce8e9aa1c4cb81460cf8de0
MikaelisBest <mikaelfazal123@gmail.com>
2025-07-11 21:54:04 +0500

    Portfolio

---
+++ js/theme.js
@@ -0,0 +1,7 @@
+const toggleBtn = document.getElementById('theme-toggle');
+
+if (toggleBtn) {
+  toggleBtn.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+  });
+}
